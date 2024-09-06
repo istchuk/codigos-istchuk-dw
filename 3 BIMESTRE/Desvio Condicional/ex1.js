980cc119193d7fdb029ccfdd4aa8961f8b859890
istchuk <gabrielistchuk@alunos.utfpr.edu.br>
2024-09-06 14:46:33 -0300

    atividade condicional

---
+++ 3 BIMESTRE/Desvio Condicional/ex1.js
@@ -0,0 +1,26 @@
+let condicao1 = true
+let condicao2 = false
+let condicao3 = true
+
+console.log("1")
+console.log("2")
+
+if (condicao1 == true) {
+    if (condicao2 == true) {
+       console.log("C") 
+    }
+    else {
+        console.log("D")
+    }
+}
+else {
+    if (condicao3 == true){
+        console.log("A")
+    }
+    else{
+        console.log("B")
+    }
+}
+
+console.log("3")
+console.log("4")
---
+++ 3 BIMESTRE/Desvio Condicional/ex2.js
@@ -0,0 +1,32 @@
+let condicao1 = true
+let condicao2 = true
+let condicao3 = false
+
+console.log("A")
+console.log("B")
+
+if (condicao1 == true){
+    console.log("A")
+    console.log("3")
+    console.log("4")
+}
+
+else {
+    if (condicao2 == false){
+        console.log("D")
+        console.log("3")
+        console.log("4")
+    }
+
+    else {
+        if (condicao3 == true){
+            console.log("B")
+            console.log("3")
+            console.log("4")
+        }
+
+        else {
+            console.log("C")
+        }
+    }
+}
